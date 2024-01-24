989aa611b4951326bfd3cb1a9bdc64f7acbec561
cherrylptl <156246897+cherrylptl@users.noreply.github.com>
2024-01-24 16:21:39 -0500

    New logo Added of conestoga
    The new black logo replaced with the white loco.

---
+++ assignment1/assignment1/ViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var count: Int = 0
+    var toggle: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    @IBOutlet weak var displayLable: UILabel!
+    
+    @IBAction func onTapMinusButton(_ sender: Any) {
+        
+        count = toggle ? count - 2 : count - 1
+        
+        displayLable.text=String(count)
+    }
+    
+    @IBAction func onTapPlusButton(_ sender: Any) {
+        
+        count = toggle ? count + 2 : count + 1
+          
+        displayLable.text=String(count)
+    }
+    
+    @IBAction func onTapStepButton(_ sender: Any) {
+        toggle = true
+    }
+    
+    @IBAction func onTapResetButton(_ sender: Any) {
+        
+        toggle = false
+        count = 0
+        displayLable.text=String(count)
+        
+    }
+    
+}
+
